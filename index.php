a71cb83e1296da75614ce56592998a564fdb10f0
Tom RUIZ <tom.ruiz@ynov.com>
2018-12-10 15:40:24 +0100

    premiere version du site

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   <title>Portfolio Tom Ruiz</title>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="/assets/style/style.css">
+
+	 <link rel="icon" type="image/png" href="assets/image/favicon.jpg">
+
+</head>
+
+<body>
+
+
+	<nav>
+		<a class="active" href="index.php#home"><img class="logo" src="/assets/image/homelogo.png"></a>
+		<a href="index.php#sound">A propos</a>
+		<a href="index.php#cv1">Curriculum vitæ</a>
+		<a href="index.php#aboutme">Contact</a>
+	</nav>
+
+
+<main>
+
+	<div id="home">
+		<div class="middle-logo">
+			<h1 class="titre">Tom Ruiz</h1>
+			<hr class="ligne-noire">
+    	<h2 class="sous-titre">Portfolio</h2>
+		</div>
+</div>
+
+	<div class="icon">
+		<a target="_blank" href="https://soundcloud.com/ternaire-prod"><img src="/assets/image/soundcloud" alt=""></a>
+		<a target="_blank" href="https://www.instagram.com/tom_ternaire/?hl=fr"><img src="/assets/image/insta" alt=""></a>
+		<a target="_blank" href="#"><img src="/assets/image/facebook" alt=""></a>
+		<a target="_blank" href="https://www.youtube.com/channel/UCF1JJsmLxsvFMU9Fjq_AFXg?view_as=subscriber"><img src="/assets/image/youtube" alt=""></a>
+		<a target="_blank" href="https://twitter.com/SneaKiZz?lang=fr"><img src="/assets/image/twitter" alt=""></a>
+	</div>
+
+
+	<div id="sound">
+    <h1>A propos de moi</h1>
+
+    <p>Mon nom est Tom Ruiz</p><br/>
+    <p>J'étudie à Ynov Aix-En-Provence où j'y prépare un bachelor informatique</p><br/>
+    <p>Je compte me spécialiser dans le développement</p><br/>
+    <p>J'ai longtemps pratiqué le langage C. J'ai aussi des bases en Python et en HTML/CSS</p><br/>
+    <p>Je suis passioné d'informatique et de musique</p><br/>
+    <p>Vous pouvez d'ailleurs retrouver les sons que je produis ici :</p> <a target="_blank" href="https://soundcloud.com/ternaire-prod"><img src="/assets/image/soundcloud" alt=""></a>
+
+</div>
+
+
+<div id="cv1">
+
+  <div class="header">
+  <h2>Curriculum Vitae</h2>
+  <h2>Tom Ruiz</h2>
+</div>
+
+<div class="row">
+  <div class="column side">Compétences :</br></br>
+
+C/C++ </br></br>
+
+Python</br></br>
+Html/CSS</br></br>
+VBA</br></br>
+  </div>
+
+  <div class="column middle">Column</div>
+  <div class="column side" style="background-color:#ccc;"></div>
+</div>
+
+<div class="footer">
+  <p>Footer</p>
+</div>
+
+
+
+</div>
+
+<div id="cv2">
+
+
+</div>
+
+<div id="aboutme">
+
+  <div class="w3-light-grey">
+    <div class="w3-blue" style="width:75%"></div>
+  </div>
+
+
+</div>
+
+</main>
+
+<footer>
+
+</footer>
+<script src="/assets/script/jquery.js"></script>
+<script type="text/javascript" src="/assets/script/smooth.js"></script>
+
+</body>
+</html>
